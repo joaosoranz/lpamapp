4a64bbd1cb11d48319796764880ab12ccaf80494
João Soranz <joaosoranz@yahoo.com.br>
2019-02-04 12:04:18 +1000

    First commit

---
+++ app/src/main/java/canterbury/lpamapp/MainActivity.java
@@ -0,0 +1,24 @@
+package canterbury.lpamapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebSettings;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webview = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = webview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webview.setWebViewClient(new WebViewClient());
+        webview.loadUrl("https://lpaecommsphp.azurewebsites.net/");
+    }
+}
